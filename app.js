b5514103e6295141119a87cc710f0e6b10b5ea52
billmun <billmunkacsy@gmail.com>
2022-08-10 18:22:42 +0000

    finished route test specs

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 module.exports = app; // this line is only used to make testing easier.
+const morgan = require('morgan')
+const todos = require('./models/express-models/todos')
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 // remember to plug in your router and any other middleware you may need here (i.e. body parser, mounting any router-level middleware, etc.)
+
+const routes = require('./routes/index')
+app.use('/users', routes)
 
 app.use((err, req, res, next) => {
   res.sendStatus(err.status);
 });
+
+// app.listen(8080, ()=>{
+//     console.log('App listening in port 8080')
+// })
 
 if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,72 @@ const router = express.Router();
 const todos = require('../models/express-models/todos');
 module.exports = router;
 
+
 // write your routes here. Feel free to split into multiple files if you like.
+
+router.post('/:name/tasks', async (req,res,next)=>{
+    try{
+        const body = await req.body
+        const name = req.params.name
+        const add = await todos.add(name,body)
+        const taskList = await todos.list(name)
+        if(taskList[0].content===''){
+            return res.sendStatus(400)
+        }
+        return res.status(201).send(taskList[0])
+    }
+    catch (e){
+        next(e)
+    }
+})
+
+router.put('/:name/tasks/:index', async (req,res,next)=>{
+    try{
+        const name = req.params.name
+        const idx = req.params.index
+        const complete = await todos.complete(name,idx)
+        return res.sendStatus(200)
+        done()
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+router.delete('/:name/tasks/:index', async (req, res, next)=>{
+    try{
+        const name = req.params.name
+        const idx = req.params.index
+        const remove = await todos.remove(name,idx)
+        return res.sendStatus(204)
+        done()
+    }
+    catch(e){
+        next(e)
+    }
+})
+
+router.get('/', async (req,res,next)=>{
+    try{
+        const people = await todos.listPeople() 
+        return res.send(people)
+    }
+    catch (e){
+        next(e)
+    }
+})
+
+router.get('/:name/tasks', async(req,res,next)=>{
+    try{
+        let name = req.params.name
+        const taskList = await todos.list(name)
+        if(taskList===undefined){
+        res.status(404)
+        }
+       res.send(taskList)
+    }
+    catch (e){
+        next(e)
+    }
+})
+
